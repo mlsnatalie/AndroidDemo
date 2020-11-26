951614d062c8445f782192e233c7cab6c57f3cd6
mlsnatalie <mlsnatalie@163.com>
2020-11-26 15:17:09 +0800

    jetPack

--- app/src/main/java/com/example/androiddemo/jetPack/NewJetPackActivity.kt
+++ app/src/main/java/com/example/androiddemo/jetPack/NewJetPackActivity.kt
@@ -1,9 +1,18 @@
 package com.example.androiddemo.jetPack
 
+import android.content.Context
 import android.database.DatabaseUtils
+import android.location.Location
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.bumptech.glide.Glide
 import com.example.androiddemo.R
 import com.example.androiddemo.databinding.ActivityNewJetpackBinding
 
@@ -16,11 +25,38 @@ import com.example.androiddemo.databinding.ActivityNewJetpackBinding
  */
 class NewJetPackActivity : AppCompatActivity() {
 
+    private val userModel: UserModel by viewModels()
+    private lateinit var myLayoutLocationListener: MyLocationListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_new_jetpack)
+        val binding: ActivityNewJetpackBinding = ActivityNewJetpackBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val binding: ActivityNewJetpackBinding = DataBindingUtil.setContentView(this, R.layout.activity_new_jetpack)
-        binding.user = User("xys", "")
+//        val binding: ActivityNewJetpackBinding = DataBindingUtil.setContentView(this, R.layout.activity_new_jetpack)
+//        binding.user = User("xys", "")
+        binding.viewmodel = userModel
+
+        myLayoutLocationListener = MyLocationListener(this) {
+            location -> {}
+        }
+
+        lifecycle.addObserver(myLayoutLocationListener)
+    }
+
+    @BindingAdapter("imageUrl")
+    fun loadImage(view: ImageView, url: String) {
+        Glide.with(this)
+            .load(url)
+            .into(view)
+    }
+
+    internal class MyLocationListener(private val context: Context, private val callback: (Location) -> Unit): LifecycleObserver {
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+        fun start() {}
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+        fun stop() {}
     }
 }
---
+++ app/src/main/java/com/example/androiddemo/jetPack/StockManager.kt
@@ -0,0 +1,15 @@
+package com.example.androiddemo.jetPack
+
+import java.math.BigDecimal
+
+class StockManager(symbol: String) {
+    fun requestPriceUpdates(listener: (BigDecimal) -> Unit) {
+
+    }
+
+    fun removeUpdates(listener: (BigDecimal) -> Unit) {
+
+    }
+
+
+}
