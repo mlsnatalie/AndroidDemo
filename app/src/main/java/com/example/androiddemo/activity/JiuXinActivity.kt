b705aa247e7020626a0f1fd1c73db0da0c6711da
mlsnatalie <mlsnatalie@163.com>
2021-05-26 19:13:45 +0800

    协程

---
+++ app/src/main/java/com/example/androiddemo/activity/JiuXinActivity.kt
@@ -0,0 +1,10 @@
+package com.example.androiddemo.activity
+
+/**
+ * @author: mlsnatalie
+ * @date: 2021/5/26 3:31 下午
+ * @UpdateDate: 2021/5/26 3:31 下午
+ * email：mlsnatalie@163.com
+ * description：
+ */class JiuXinActivity {
+}
